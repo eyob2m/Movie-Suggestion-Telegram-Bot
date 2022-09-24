905d353f0bad1b91f9fe8badb0502ed518b36b59
eyob2m <105048361+eyob2m@users.noreply.github.com>
2022-09-24 14:15:02 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+from bs4 import BeautifulSoup
+import requests
+import telebot
+
+API_KEY = '5636091834:AAFP1TLallmgSccD-w1v1aJ8ta8jq9n_x_w'
+bot = telebot.TeleBot(API_KEY)
+channel = '@BoxOfficeY'
+
+url = requests.get('https://www.imdb.com/chart/boxoffice')
+soup = BeautifulSoup(url.content, 'html.parser')
+table = soup.find('tbody')
+list = table.find_all('tr')
+
+@bot.message_handler(commands=['boxo'])
+
+def boxo(message):
+    for top in list:
+      
+			  
+        h = top.find('td', class_='titleColumn')
+        m = top.find('td', class_='ratingColumn')
+        k = top.find(class_='secondaryInfo')
+        w = top.find(class_='weeksColumn')
+
+        print("____________________\n" +" >> " +h.a.text + "  <<\n________________\n This Week : " + m.text.strip() +
+              "\n Total Gross : " + k.text.strip() + "\n Weeks : " +
+              w.text.strip())
+
+        bot.reply_to(
+            message, "___________________________\n"+"  >>  "+ h.a.text + "  << \n________________\n This Week : " +
+            m.text.strip() + "\n Total Gross : " + k.text.strip() +
+            "\n Weeks : " + w.text.strip())
+			  
+        bot.send_message(
+            channel, "___________________________\n"+"  >>  "+ h.a.text + "  << \n________________\n This Week : " +
+            m.text.strip() + "\n Total Gross : " + k.text.strip() +
+            "\n Weeks : " + w.text.strip())
+
+bot.polling()
