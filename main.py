4355cd1415b2bd42463bc4166081ba9301c7c3ca
eyob2m <105048361+eyob2m@users.noreply.github.com>
2022-09-24 14:56:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import telebot
 API_KEY = '5636091834:AAFP1TLallmgSccD-w1v1aJ8ta8jq9n_x_w'
 bot = telebot.TeleBot(API_KEY)
 channel = '@BoxOfficeY'
-
+n = 1 
 url = requests.get('https://www.imdb.com/chart/boxoffice')
 soup = BeautifulSoup(url.content, 'html.parser')
 table = soup.find('tbody')
@@ -18,7 +18,7 @@ bot.send_message(channel,date)
 def boxo(message):
     	
     for top in list:
-      
+       
 			  
         h = top.find('td', class_='titleColumn')
         m = top.find('td', class_='ratingColumn')
@@ -27,8 +27,9 @@ def boxo(message):
        
      
         bot.send_message(
-            channel, "___________________________\n"+"  >>  "+ h.a.text + "  << \n________________\n This Week : " +
+            channel, "___________________________\n"+n"."+ h.a.text + "  << \n________________\n This Week : " +
             m.text.strip() + "\n Total Gross : " + k.text.strip() +
             "\n Weeks : " + w.text.strip())
+	n+=1
 
 bot.polling()
