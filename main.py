bbcb0cef64d5965d5b3a6cc9d91777b7151c2c59
eyob2m <105048361+eyob2m@users.noreply.github.com>
2022-09-27 07:05:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def post():
 	
 
 schedule.every().tuesday.at("13:57").do(link)
-schedule.every().tuesday.at("14:04").do(datefun)
+schedule.every().tuesday.at("14:07").do(datefun)
 schedule.every().tuesday.at("13:56").do(post)
 while True:
  
