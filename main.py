365713e850c37b2ae0f78bea6a11231950f2d9f8
eyob2m <105048361+eyob2m@users.noreply.github.com>
2022-09-29 02:38:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,20 @@ table = soup.find('tbody')
 list = table.find_all('tr')
 date = soup.find('h4')
 
+def random():
 	
+    urll = requests.get('https://ssnvalidator.net/test/get-movie.php')
+
+    sup = urll.json()
+    t= " Title :   "+sup["title"]
+    y=" Year :   "+sup["year"]
+    p =" Plot :   "+sup["description"]
+    g = t+'\n'+y+'\n'+p
+    s = requests.get(sup["image"]).content
+    photo = open("image.png", 'wb')
+    photo.write(s)
+    bot.send_photo(channel,photo, caption=g)
+
 def link():
 	bot.send_message(channel,' \U0001f31a For Updated Information || @BoxOfficeY ||')
 
@@ -43,6 +56,7 @@ def post():
 schedule.every().tuesday.at("15:30:30").do(link)
 schedule.every().tuesday.at("15:30:00").do(datefun)
 schedule.every().tuesday.at("15:30:05").do(post)
+schedule.every(30).seconds.do(random)
 while True:
  
     schedule.run_pending()
